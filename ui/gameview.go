660a45533cc9dd7b4cfd92653d57b20840840c99
Kenko Nakamura <kenkou.n@gmail.com>
2018-12-29 01:48:21 +0900

    Simplified

--- ui/gameview.go
+++ ui/gameview.go
@@ -33,32 +33,12 @@ func (view *GameView) Enter() {
 	view.window.SetKeyCallback(view.onKey)
 
 	view.console.Reset()
-	// // load state
-	// if err := view.console.LoadState(savePath(view.hash)); err == nil {
-	// 	return
-	// } else {
-	// view.console.Reset()
-	// }
-	// // load sram
-	// cartridge := view.console.Cartridge
-	// if cartridge.Battery != 0 {
-	// 	if sram, err := readSRAM(sramPath(view.hash)); err == nil {
-	// 		cartridge.SRAM = sram
-	// 	}
-	// }
 }
 
 func (view *GameView) Exit() {
 	view.window.SetKeyCallback(nil)
 	view.console.SetAudioChannel(nil)
 	view.console.SetAudioSampleRate(0)
-	// // save sram
-	// cartridge := view.console.Cartridge
-	// if cartridge.Battery != 0 {
-	// 	writeSRAM(sramPath(view.hash), cartridge.SRAM)
-	// }
-	// // save state
-	// view.console.SaveState(savePath(view.hash))
 }
 
 func (view *GameView) Update(t, dt float64) {
